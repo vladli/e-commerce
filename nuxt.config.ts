b825886be7d984ee194f62c048a33b96b1790a45
Vladislav Li <vladislav.li911@gmail.com>
2024-03-26 23:43:20 +0900

    Update app.vue, package.json, and nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["/main.css"],
+  app: {
+    head: {
+      title: "My Nuxt App",
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
